27cbc6d2bae430e3961a59e65f6a7c76ec160032
Sakuralotus <45547733+Sakuralotus@users.noreply.github.com>
2020-03-08 18:45:57 +0800

    Create MainBook.java

---
+++ MainBook.java
@@ -0,0 +1,23 @@
+import com.company.Book;
+
+public class MainBook {
+    public static void main(String[] args) {
+        Book book1= new Book();
+        Book book2 = new Book();
+        Book book3= new Book();
+        Book book4= new Book();
+        Book book5= new Book();
+        book1.setName("King Author");
+        book1.setAuthor("Author");
+        book1.setISBN("465465165486146846");
+        book1.setPrice("199");
+        book1.setpublishday("2020/2/29");
+        System.out.println(book1.getName());
+        System.out.println(book1.getAuthor());
+        System.out.println(book1.getISBN());
+        System.out.println(book1.getPrice());
+        System.out.println(book1.getpublishday());
+        book1.showInfo();
+    }
+}
+
