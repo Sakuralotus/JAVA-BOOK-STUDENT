303ffa405f2b387e1d352b3bdc2b912c3567f5b4
Sakuralotus <45547733+Sakuralotus@users.noreply.github.com>
2020-03-11 19:45:47 +0800

    Update MainBook.java
    Update again

--- MainBook.java
+++ MainBook.java
@@ -4,22 +4,45 @@ public class MainBook {
     public static void main(String[] args) {
        
         Book book1= new Book();
-        Book2 book2 = new Book2();
-        Book3 book3= new Book3();
-       
+Book book2= new Book();
+Book book3= new Book();
+Book book4= new Book();
+Book book5= new Book();
+        Book2 book6 = new Book2();
+Book2 book7 = new Book2();
+Book2 book8 = new Book2();
+Book2 book9 = new Book2();
+Book2 book10 = new Book2();
+        Book3 book11= new Book3();
+Book3 book12= new Book3();
+       Book3 book13= new Book3();
+Book3 book14= new Book3();
+Book3 book15= new Book3();
         book1.setName("King Author");
-        book1.setAuthor("Author");
-        book1.setISBN("465465165486146846");
-        book1.setPrice("199");
-        book1.setpublishday("2020/2/29");
-        System.out.println(book1.getName());
-        System.out.println(book1.getAuthor());
-        System.out.println(book1.getISBN());
-        System.out.println(book1.getPrice());
-        System.out.println(book1.getpublishday());
+        book2.setAuthor("Author");
+        book3.setISBN("465465165486146846");
+        book4.setPrice("199");
+        book5.setpublishday("2020/2/29");
+       // System.out.println(book1.getName());
+       // System.out.println(book1.getAuthor());
+       // System.out.println(book1.getISBN());
+       // System.out.println(book1.getPrice());
+       // System.out.println(book1.getpublishday());
         book1.showInfo();
 book2.showInfo();
 book3.showInfo();
+book4.showInfo();
+book5.showInfo();
+book6.showInfo();
+book7.showInfo();
+book8.showInfo();
+book9.showInfo();
+book10.showInfo();
+book11.showInfo();
+book12.showInfo();
+book13.showInfo();
+book14.showInfo();
+book15.showInfo();
     }
 }
 
